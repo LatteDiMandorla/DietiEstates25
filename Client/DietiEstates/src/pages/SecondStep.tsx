08a8ee9e750c7ed579c8c43125fc3bff21f5ca00
Sderr12 <andrea.sidero6@gmail.com>
2025-01-05 13:04:02 +0100

    minor changes

--- Client/DietiEstates/src/pages/SecondStep.tsx
+++ Client/DietiEstates/src/pages/SecondStep.tsx
@@ -4,6 +4,14 @@ import useRangeCounter from '../hooks/useRangeCounter';
 
 
 export const SecondStep = () => {
+
+    const HandleOptionClick = (option : "Casa" | "Villa" | "Appartamento" | "Baita") => {
+        console.log(option);
+    }
+
+    const steps = 3;
+    const {counter: step, prev, next} = useRangeCounter(steps);
+
     return (
 
         <div className=" bg-white">
@@ -27,6 +35,11 @@ export const SecondStep = () => {
                 </div>
          </header>
 
+
+         <div className="my-6 flex justify-center">
+            <StepIndicator selected={1} steps={steps} />
+         </div>
+
          
          <div className="bg-white w-full h-screen mt-28 flex justify-center">
             
@@ -39,24 +52,28 @@ export const SecondStep = () => {
 
                     {/* First Part */}
                     <div className="grid grid-cols-2 gap-3 mt-10">
-                        <button className="bg-blue-200 p-20 rounded-lg shadow-md">
-                            Casa
+                        <button onClick={() => HandleOptionClick("Casa")}
+                                className="bg-blue-200 p-20 rounded-lg shadow-md hover:scale-95 transition-transform">
+                                Casa
                         </button>
-                        <button className="bg-blue-200 p-20 rounded-lg shadow-md"> 
-                            Villa
+                        <button onClick={() => HandleOptionClick("Villa")}
+                                className="bg-blue-200 p-20 rounded-lg shadow-md hover:scale-95 transition-transform"> 
+                                Villa
                         </button>
-                        <button className="bg-blue-200 p-20 rounded-lg shadow-md">
-                            Appartamento
+                        <button onClick={() => HandleOptionClick("Appartamento")}
+                                className="bg-blue-200 p-20 rounded-lg shadow-md hover:scale-95 transition-transform">
+                                Appartamento
                         </button>
-                        <button className="bg-blue-200 p-20 rounded-lg shadow-md">
-                            Baita
+                        <button onClick={() => HandleOptionClick("Baita")}
+                                className="bg-blue-200 p-20 rounded-lg shadow-md hover:scale-95 transition-transform">
+                                Baita
                         </button>
                     </div>
 
                 </div>
 
                 {/* Second Part */}
-                <div className="bg-white w-1/2 rounded-lg p-5 shadow-md">
+                <div className="bg-white w-1/2 rounded-lg p-5 h-3/4 shadow-md">
                     <h1 className="text-blue-950 font-mono text-center underline text-2xl">
                         Delinea l'immobile
                     </h1>
