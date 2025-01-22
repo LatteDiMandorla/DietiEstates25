ab03872937513cbea89d62f13de73a108dffa9e9
LatteDiMandorla <giovannidg02@gmail.com>
2025-01-22 19:54:05 +0100

    Add EnergyEfficiency, CaratteristicsAndDescription

---
+++ Client/src/components/EnergyEfficiency.tsx
@@ -0,0 +1,11 @@
+export const EnergyEffiency = () => {
+    return (
+        <div className="relative left-24 flex flex-col items-center h-12 w-[40rem] bg-[#FFFFFF] shadow-[0px_4px_10px_rgba(0,0,0,0.25)] rounded-full">
+            <span> 
+                Efficienza Energetica
+            </span>
+
+
+        </div>
+    )
+}
